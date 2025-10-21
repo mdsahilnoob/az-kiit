a1e6bdee54c945785b2cffd218013611255e18bb
KingsukNandi <kingsuk24nandi@gmail.com>
2025-10-21 22:25:15 +0530

    setup analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/global/Navbar";
 import Footer from "@/components/global/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "AlgoZenith - KIIT Chapter",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en" className="overflow-x-hidden">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body className={`antialiased bg-[#001d2f] text-white overflow-x-hidden`}>
+        <Analytics />
         <div className="bg-[url(/backgrounds/circuit.png)] bg-repeat h-full w-full fixed -z-10 top-0 left-0"></div>
         <Navbar />
         {children}
